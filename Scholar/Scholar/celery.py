19a36f73a2824b15da9c42b2ffbdb44a5ca62801
暗中观察|.･｀) <2868470542@qq.com>
2022-10-22 21:22:01 +0800

    fix : fix a bug of celery

--- Scholar/Scholar/celery.py
+++ Scholar/Scholar/celery.py
@@ -8,8 +8,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Scholar.settings')
 
 # 创建 celery
 app = Celery('Scholar',
-             broker='redis://:' + redis_PASSWORD + '@' + redis_HOST + '/4',
-             backend='redis://:' + redis_PASSWORD + '@' + redis_HOST + '/5')
+             broker='redis://:' + redis_PASSWORD + '@' + redis_HOST + ":" + str(redis_PORT) + '/1',
+             backend='redis://:' + redis_PASSWORD + '@' + redis_HOST + ":" + str(redis_PORT) + '/1')
 
 # 配置每个应用的 worker 工作
 app.autodiscover_tasks(settings.INSTALLED_APPS)
