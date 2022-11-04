b11f0e02079cde05a0ca60420997b1936d802bc5
anjingkun <853048903@qq.com>
2022-11-04 09:57:17 +0800

    增加认领门户时作者需要存在

--- Scholar/form/views.py
+++ Scholar/form/views.py
@@ -5,7 +5,7 @@ from utils.Sending_utils import *
 # Create your views here.
 from django.core.cache import cache
 
-
+open_alex = OpenAlex("853048903@qq.com")
 @login_checker
 def user_claim_author(request):  # 用户申请认领门户
     if request.method == 'POST':
@@ -14,6 +14,10 @@ def user_claim_author(request):  # 用户申请认领门户
         content = data_json.get('content', '默认申请内容')
         user_id = request.user_id
         author_id = data_json.get('author_id', '')
+        try:
+            open_alex.get_single_author("A249906320")
+        except:
+            return JsonResponse({'result': 2, 'message': '申请的作者不存在'})
         form_handling_key, form_handling_dic = cache_get_by_id('form', 'Form_list', 0)  # 从cache中获得正在处理的申请的id列表
         new_claim = models.Form.objects.create(author_id=author_id, content=content, user_id=user_id)
         cache_set_after_create('form', 'Form', new_claim.id, new_claim.to_dic())  # 将刚刚生成的表单放在redis中
