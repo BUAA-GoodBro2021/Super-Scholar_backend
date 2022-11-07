9c013290b3330aaf89c0a86a3c8c860d57035122
anjingkun <853048903@qq.com>
2022-11-08 00:40:42 +0800

    change:修改数据库字段

--- Scholar/form/models.py
+++ Scholar/form/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 
-class Form_list(models.Model):
-    Form_id_list = models.TextField('未处理的申请的id的列表', max_length=1024, default='[]')
+class FormList(models.Model):
+    form_id_list = models.TextField('未处理的申请的id的列表', max_length=1024, default='[]')
     id = models.IntegerField('申请处理状态', primary_key=True)  # 0表示正在审批，1表示申请通过，2表示申请拒绝
 
     def to_dic(self):
         return {
             'id': self.id,
-            'Form_id_list': eval(self.Form_id_list),
+            'Form_id_list': eval(self.form_id_list),
         }
 
 
