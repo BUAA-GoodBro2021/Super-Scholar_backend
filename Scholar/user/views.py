5562c5022ad2946fda72d159695089a6f39d5d14
暗中观察|.･｀) <2868470542@qq.com>
2022-11-27 20:27:50 +0800

    feat : 增加查看用户自己门户详情的接口

--- Scholar/user/views.py
+++ Scholar/user/views.py
@@ -214,39 +214,52 @@ def get_user(request):
         return JsonResponse(result)
 
 
-# # 查看用户自己的门户信息
-# @login_checker
-# def get_works(request):
-#     if request.method == 'POST':
-#
-#         # 获取表单信息
-#         data_json = json.loads(request.body.decode())
-#         # 获取到需要查询的页数以及每页多少个
-#         pages = data_json.get('pages', '')
-#         per_page = data_json.get('per_page', '')
-#
-#         # 获取用户id
-#         user_id = request.user_id
-#         # 获取信息
-#         user_key, user_dict = cache_get_by_id('user', 'user', user_id)
-#
-#         # 获取到作者的 open_alex_id
-#         is_professional = user_dict['is_professional']
-#         open_alex_id = user_dict['open_alex_id']
-#
-#         # 判断是否为已经认领门户的用户
-#         if is_professional != 1:
-#             result = {'result': 0, 'message': r"您暂时没有认领门户或者正在申请门户，请成功认领门户之后再尝试！"}
-#             return JsonResponse(result)
-#
-#         # 通过open_alex_id获取到其文章列表
-#         open_alex = OpenAlex("zhouenshen@buaa.edu.cn")
-#         user_work_list = list(
-#             open_alex.get_list_of_works(filters={"author.id": open_alex_id}, pages=[pages, ], per_page=per_page))
-#
-#         for every_user_work in user_work_list:
-#             if every_user_work['open_access'].get('oa_url', '')
-#
-#     else:
-#         result = {'result': 0, 'message': r"请求方式错误！"}
-#         return JsonResponse(result)
+# 查看用户自己的门户信息
+@login_checker
+def get_user_info(request):
+    if request.method == 'POST':
+
+        # 获取表单信息
+        data_json = json.loads(request.body.decode())
+        # 获取到需要查询的页数以及每页多少个
+        pages = data_json.get('page', '')
+        per_page = data_json.get('per_page', '')
+
+        # 获取用户id
+        user_id = request.user_id
+        # 获取信息
+        user_key, user_dict = cache_get_by_id('user', 'user', user_id)
+
+        # 获取到作者的 open_alex_id
+        is_professional = user_dict['is_professional']
+        open_alex_id = user_dict['open_alex_id']
+
+        # 判断是否为已经认领门户的用户
+        if is_professional != 1:
+            result = {'result': 0, 'message': r"您暂时没有认领门户或者正在申请门户，请成功认领门户之后再尝试！"}
+            return JsonResponse(result)
+
+        # 通过open_alex_id获取到其文章列表
+        open_alex = OpenAlex("zhouenshen@buaa.edu.cn")
+        # 获取自己的作品详情
+        user_info = list(
+            open_alex.get_list_of_works(filters={"author.id": open_alex_id}, pages=[pages, ], per_page=per_page))
+
+        # 自己作品列表长度
+        user_info_length = len(user_info[0]['results'])
+        for i in range(user_info_length):
+            # 如果没有原文
+            if not user_info[0]['results'][i]['open_access'].get('oa_url', False):
+                try:
+                    # 说明上传了PDF,且该PDF没有删除
+                    work_key, work_dic = cache_get_by_id('work', 'work',
+                                                         user_info[0]['results'][i]['id'].split('/')[-1])
+                    user_info[0]['results'][i]['open_access']['is_oa'] = True
+                    user_info[0]['results'][i]['open_access']['oa_url'] = work_dic['url']
+                except:
+                    pass
+        result = {'result': 1, 'message': r"获取门户信息成功！", 'user_info': user_info}
+        return JsonResponse(result)
+    else:
+        result = {'result': 0, 'message': r"请求方式错误！"}
+        return JsonResponse(result)
