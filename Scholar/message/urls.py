98cb23e0143dc61817f31ffa182f3db0490b3af8
anjingkun <853048903@qq.com>
2022-11-28 02:06:18 +0800

    add:增加上传pdf站内信

--- Scholar/message/urls.py
+++ Scholar/message/urls.py
@@ -4,6 +4,6 @@ from message import views
 
 urlpatterns = [
     path('look_message_list', views.look_message_list),
-
+    path('delete_message', views.delete_message),
     # path("get_index_data_view", views.get_index_data),
 ]
