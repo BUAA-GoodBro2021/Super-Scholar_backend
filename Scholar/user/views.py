4afc3220bd83472640bdf0c3d274fa8245890563
anjingkun <853048903@qq.com>
2022-12-01 14:18:07 +0800

    add:增加密码强度监测

--- Scholar/user/views.py
+++ Scholar/user/views.py
@@ -14,6 +14,42 @@ def test(request):
     return HttpResponse("Hello world!")
 
 
+def check_number(password):
+    for c in password:
+        if c.isnumeric():
+            return True
+
+
+def check_letter(password):
+    for c in password:
+        if 'a' <= c <= 'z' or 'A' <= c <= 'Z':
+            return True
+
+
+def check_mark(password):
+    for c in password:
+        if not (c.isnumeric() or 'a' <= c <= 'z' or 'A' <= c <= 'Z'):
+            return True
+
+
+def check_legal(password):
+    if len(password) < 8 or len(password) > 16:
+        return {'result': 0, 'message': '长度需为8-16个字符,请重新输入。'}
+    else:
+        for i in password:
+            if 0x4e00 <= ord(i) <= 0x9fa5 or ord(i) == 0x20:  # Ox4e00等十六进制数分别为中文字符和空格的Unicode编码
+                return {'result': 0, 'message': '不能使用空格、中文，请重新输入。'}
+        else:
+            key = 0
+            key += 1 if check_number(password) else 0
+            key += 1 if check_letter(password) else 0
+            key += 1 if check_mark(password) else 0
+            if key >= 2:
+                return {'result': 1, 'message': '密码强度合适'}
+            else:
+                return {'result': 0, 'message': '至少含数字/字母/字符2种组合，请重新输入。'}
+
+
 # 用户注册
 # TODO 密码强度检测。
 def register(request):
@@ -49,7 +85,9 @@ def register(request):
                 'message': r'两次密码不一致!'
             }
             return JsonResponse(result)
-
+        message = check_legal(password1)
+        if message['result'] != 1:
+            return JsonResponse(message)
         user = User.objects.create(
             username=username,
             email=email,
