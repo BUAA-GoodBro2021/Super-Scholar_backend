817b7a8a6d73ece8767fc0b8b8a3dbbb63163ff3
暗中观察|.･｀) <2868470542@qq.com>
2022-12-01 11:31:32 +0800

    fix : fix a bug of updating history

--- Scholar/history/tasks.py
+++ Scholar/history/tasks.py
@@ -2,11 +2,9 @@ from Scholar.celery import app
 from history.models import History
 
 
-
 @app.task
-def celery_update_history(user_id, history_list):
+def celery_update_history(user_id, history_list_string):
     history = History.objects.get(id=user_id)
-    history_list_string = '|'.join(history_list)
     history.history_list = history_list_string
     history.save()
     return history.to_dic()
--- Scholar/history/views.py
+++ Scholar/history/views.py
@@ -3,24 +3,31 @@ import json
 from django.core.cache import cache
 from django.http import JsonResponse
 from history.tasks import *
+from utils.Login_utils import login_checker
 
 from utils.Redis_utils import cache_get_by_id
 
 
+@login_checker
 def update_history(request):
     if request.method == 'POST':
         user_id = request.user_id
         # 获取表单信息
         data_json = json.loads(request.body.decode())
-        history_list = data_json.get('history_list', [])
+
+        history_list_string = data_json.get('history_list', "")
         history_key, history_dict = cache_get_by_id('history', 'history', user_id)
+
+        # 分割成字符串数组
+        history_list = history_list_string.split('|')
+        history_list = [history_list_info for history_list_info in history_list if history_list_info != ""]
 
         # 修改缓存
         history_dict['history_list'] = history_list
         cache.set(history_key, history_dict)
 
         # 修改数据库
-        celery_update_history.delay(user_id, history_list)
+        celery_update_history.delay(user_id, history_list_string)
 
     else:
         result = {'result': 0, 'message': r"请求方式错误！"}
