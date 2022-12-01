fced83bec2806f1afcacd2982a9faf0c8df38e1d
暗中观察|.･｀) <2868470542@qq.com>
2022-12-01 23:29:21 +0800

    fix: fix a bug of history list

--- Scholar/history/views.py
+++ Scholar/history/views.py
@@ -15,11 +15,9 @@ def update_history(request):
         # 获取表单信息
         data_json = json.loads(request.body.decode())
 
-        history_list_string = data_json.get('history_list', "")
+        history_list = data_json.get('history_list', "")
         history_key, history_dict = cache_get_by_id('history', 'history', user_id)
 
-        # 分割成字符串数组
-        history_list = history_list_string.split('|')
         history_list = [history_list_info for history_list_info in history_list if history_list_info != ""]
 
         # 修改缓存
@@ -27,7 +25,7 @@ def update_history(request):
         cache.set(history_key, history_dict)
 
         # 修改数据库
-        celery_update_history.delay(user_id, history_list_string)
+        celery_update_history.delay(user_id, history_list)
         result = {'result': 1, 'message': r"更新历史记录成功！", "history_list": history_list}
         return JsonResponse(result)
 
