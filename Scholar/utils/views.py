3b9af63c395c45b57e82ec4c6771b40822516f29
anjingkun <853048903@qq.com>
2022-12-05 17:51:58 +0800

    change:邮箱可以重复使用

--- Scholar/utils/views.py
+++ Scholar/utils/views.py
@@ -53,10 +53,10 @@ def active(request, token):
         email = payload.get('email')
 
         # 防止同一邮箱反复激活
-        if User.objects.filter(email=email, is_active=True).exists():
-            content["title"] = "激活失败"
-            content["message"] = "啊偶，该邮箱已经被激活过啦，一个邮箱只能绑定一个用户哦！"
-            return render(request, 'EmailContent-check.html', content)
+        # if User.objects.filter(email=email, is_active=True).exists():
+        #     content["title"] = "激活失败"
+        #     content["message"] = "啊偶，该邮箱已经被激活过啦，一个邮箱只能绑定一个用户哦！"
+        #     return render(request, 'EmailContent-check.html', content)
 
         # 激活用户 验证邮箱
         user_dict['is_active'] = True
