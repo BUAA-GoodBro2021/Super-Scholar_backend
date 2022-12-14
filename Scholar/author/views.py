51aa9dfd837a7f40f301ecc6feefefa2f9df17d4
anjingkun <853048903@qq.com>
2022-12-14 15:51:52 +0800

    change:快排优化

--- Scholar/author/views.py
+++ Scholar/author/views.py
@@ -3,8 +3,9 @@ import json
 from django.http import JsonResponse
 
 # Create your views here.
+from form.views import open_alex
 from utils.Login_utils import login_checker
-from utils.Redis_utils import cache_get_list_by_diophila
+from utils.Redis_utils import cache_get_list_by_diophila, get_work_abstract, cache_get_by_id
 
 
 def partition(arr, low, high):
@@ -52,7 +53,7 @@ def get_relate_net(request):
                 "per_page": 200
             }
         }
-        author_works = cache_get_list_by_diophila(request_body_json)
+        author_works = get_value(request_body_json)
         cooperation_author_count = 0
         cooperation_author_list = []
         for work in author_works[0]['results']:
@@ -96,8 +97,48 @@ def get_relate_net(request):
                         print(author)
                         continue
 
-        cooperation_author_list.sort(key=lambda s:s['cooperation_author_count'])
+        cooperation_author_list.sort(key=lambda s: s['cooperation_author_count'])
 
         return JsonResponse(
             {'result': 1, 'message': '获取成功', 'cooperation_author_count': cooperation_author_count,
              'cooperation_author_list': cooperation_author_list})
+
+
+def get_value(request_body_json):
+    if request_body_json['entity_type'] == 'works':
+        value = list(open_alex.get_list_of_works(filters=request_body_json['params'].get('filter', None),
+                                                 search=request_body_json['params'].get('search', None),
+                                                 sort=request_body_json['params'].get('sort', None),
+                                                 per_page=int(request_body_json['params'].get('per_page', 25)),
+                                                 pages=[int(request_body_json['params'].get('page', 1)), ]))
+        # 自己作品列表长度
+        value_length = len(value[0]['results'])
+        for i in range(value_length):
+            if value[0]['results'][i]['abstract_inverted_index'] != None:
+                value[0]['results'][i]['abstract'] = get_work_abstract(
+                    value[0]['results'][i]['abstract_inverted_index'])
+            else:
+                value[0]['results'][i]['abstract'] = ""
+
+            # 如果 openAlex 信息中没有原文
+            if not value[0]['results'][i]['open_access'].get('is_oa', False):
+                try:
+                    # 是否上传 PDF, 如果上传并审核成功是 1, 上传正在审核是 0, 如果没有上传是 -1
+                    work_key, work_dic = cache_get_by_id('work', 'work',
+                                                         value[0]['results'][i]['id'].split('/')[-1])
+                    value[0]['results'][i]['open_access']['is_oa'] = work_dic['has_pdf']
+                    value[0]['results'][i]['open_access']['oa_url'] = work_dic['url']
+                except:
+                    value[0]['results'][i]['open_access']['is_oa'] = -1
+            # 如果 openAlex 信息中有原文，状态是 1
+            else:
+                value[0]['results'][i]['open_access']['is_oa'] = 1
+
+            # 获取 2022 的引用量
+            if len(value[0]['results'][i]['counts_by_year']) == 0 or \
+                    value[0]['results'][i]['counts_by_year'][0]['year'] != 2022:
+                value[0]['results'][i]['2022_cited_count'] = 0
+            else:
+                value[0]['results'][i]['2022_cited_count'] = value[0]['results'][i]['counts_by_year'][0][
+                    'cited_by_count']
+        return value
